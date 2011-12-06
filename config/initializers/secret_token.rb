3ac2578ca40ee098f14997cff36e4309707fad9a
João Portela <joao.portela@gmail.com>
2011-12-06 22:51:20 +0000

    Initial commit -- clean rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cmov2Server::Application.config.secret_token = '11a12d9714893b0f48cb719b238f82dd28f49ea3ee33f5d53ad15a5cfb38bb01a56abc204190e4f4097a4f156933ba0b16b88f692cb542eb6d60e1e0e515ec6e'
