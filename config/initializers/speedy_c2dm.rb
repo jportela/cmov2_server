ffec4c7bac9ee90b587e108134796814f31877b5
João Portela <joao.portela@gmail.com>
2011-12-07 15:50:56 +0000

    added cd2m configuration as an example file

--- config/initializers/speedy_c2dm.rb
+++
@@ -1,4 +0,0 @@
-C2DM_API_EMAIL = "joao.portela@gmail.com"
-C2DM_API_PASSWORD = "mypassword"
-
-SpeedyC2DM::API.set_account(C2DM_API_EMAIL, C2DM_API_PASSWORD)
---
+++ config/initializers/speedy_c2dm_example.rb
@@ -0,0 +1,4 @@
+#C2DM_API_EMAIL = "changeme@gmail.com"
+#C2DM_API_PASSWORD = "changeme"
+
+#SpeedyC2DM::API.set_account(C2DM_API_EMAIL, C2DM_API_PASSWORD)
